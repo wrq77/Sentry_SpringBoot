0e659e2719ffe4b9991473d230565163fef00528
Gabrielle Kurah <49395894+Gabicle@users.noreply.github.com>
2021-05-31 21:46:28 +0200

    Add Get devices and sensors to repository

--- Sentry/src/main/java/com/sentry/Sentry/repository/SensorRepository.java
+++ Sentry/src/main/java/com/sentry/Sentry/repository/SensorRepository.java
@@ -18,6 +18,9 @@ public interface SensorRepository extends JpaRepository<Sensor, Integer> {
 	@Query(value="Select * from sensor WHERE FK_ROOM_ID IN ( SELECT ROOM_ID FROM room WHERE FK_USER_ID = ?1)",nativeQuery=true)
 	 List<Sensor> findAll(int user_id);
 
+	@Query(value="Select * from sensor WHERE FK_ROOM_ID= ?1",nativeQuery=true)
+	List<Sensor>findSensorsByRoomRid(int rid);
+
 	//Room
 
 	@Query(value="Select count(*) from sensor_data WHERE SENSOR_STATUS = 1 AND FK_SENSOR_ID IN (Select SENSOR_ID from sensor WHERE FK_ROOM_ID IN ( SELECT ROOM_ID FROM room WHERE FK_USER_ID = ?1))",nativeQuery=true)
