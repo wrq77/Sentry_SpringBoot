0ad616fe5f4a5d6df1ee9b93671f3d0c2b3ac96a
Resminos <loic75019@hotmail.fr>
2021-06-01 22:23:32 +0200

    Add files via upload
    Added the new version of sensorDataService and sensor DataServiceimpl to get the sensor_value

--- Sentry/src/main/java/com/sentry/Sentry/service/SensorDataServiceImpl.java
+++ Sentry/src/main/java/com/sentry/Sentry/service/SensorDataServiceImpl.java
@@ -1,43 +1,64 @@
-//package com.sentry.Sentry.service;
-//
-//import java.util.List;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import com.sentry.Sentry.dao.SensorDataDAO;
-//import com.sentry.Sentry.entity.SensorData;
-//
-//@Service
-//public class SensorDataServiceImpl implements SensorDataService{
-//
-//	private SensorDataDAO sensorDataDAO;
-//
-//	 @Autowired
-//	 public SensorDataServiceImpl(SensorDataDAO theSensorDataDAO){
-//		 sensorDataDAO = theSensorDataDAO;
-//	    }
-//
-//	@Override
-//    @Transactional
-//	public List<SensorData> findAll() {
-//
-//		return sensorDataDAO.findAll();
-//
-//	}
-//
-//	@Override
-//    @Transactional
-//	public SensorData findById(int theId) {
-//		 return sensorDataDAO.findById(theId);
-//	}
-//
-//	@Override
-//    @Transactional
-//	public void save(SensorData theSensorData) {
-//		sensorDataDAO.save(theSensorData);
-//
-//	}
-//
-//}
+package com.sentry.Sentry.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import com.sentry.Sentry.entity.Sensor;
+import com.sentry.Sentry.entity.SensorData;
+import com.sentry.Sentry.repository.SensorDataRepository;
+import com.sentry.Sentry.repository.SensorRepository;
+
+@Service
+public class SensorDataServiceImpl implements SensorDataService{
+	
+	private SensorRepository sensorRepository;
+	
+	@Autowired
+	private SensorDataRepository sensorDataRepository;
+	
+	private List<Sensor> sensors;
+	private List<SensorData> sensorValueType;
+
+	
+	public SensorDataServiceImpl (SensorDataRepository sensorDataRepository){
+		this.sensorDataRepository=sensorDataRepository;	
+		
+	}
+	@Override
+	public List<SensorData> sensorValueType(int i) {
+		sensorValueType = sensorDataRepository.sensorValueType(i);
+		return sensorValueType;
+	}
+	
+	
+	
+
+	@Override
+	public List<Sensor> findAll(int user_id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<SensorData> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public SensorData findById(int theId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void save(SensorData theSensorData) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
